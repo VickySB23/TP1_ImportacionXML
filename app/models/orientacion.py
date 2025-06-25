4baf913b1c6477aebe59d48e08f451e860d7c96d
Relyckon4188 <relyckon4188@gmail.com>
2025-06-25 17:23:07 -0300

    editar models

--- app/models/orientacion.py
+++ app/models/orientacion.py
@@ -1,12 +1,16 @@
 from dataclasses import dataclass
-from app.models.especialidad import Especialidad
-from app.models.plan import Plan
-from app.models.materia import Materia
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from app.config.database import Base
 
 @dataclass(init=False, repr=True, eq=True)
-class Orientacion():
-    nombre: str
-    especialidad: Especialidad
-    plan: Plan
-    materias: Materia
-    
+class Orientacion(Base):
+    __tablename__ = 'orientaciones'
+    especialidad = Column(Integer, ForeignKey('especialidades.id'), nullable=False)
+    plan = Column(Integer, ForeignKey('planes.id'), nullable=False)
+    orientacion = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(100), nullable=False)
+
+    especialidad = relationship("Especialidad")
+    plan = relationship("Plan")
+    materias = relationship("Materia")
