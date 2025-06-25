7989f6533b702d37ca0ec3533e1d3bd187c48c42
Relyckon4188 <relyckon4188@gmail.com>
2025-06-25 18:49:10 -0300

    actualizar models.orientcion

--- app/models/orientacion.py
+++ app/models/orientacion.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
 from app.config.database import Base
+import xml.etree.ElementTree as ET
 
 @dataclass(init=False, repr=True, eq=True)
 class Orientacion(Base):
@@ -11,6 +11,16 @@ class Orientacion(Base):
     orientacion = Column(Integer, primary_key=True, autoincrement=True)
     nombre = Column(String(100), nullable=False)
 
-    especialidad = relationship("Especialidad")
-    plan = relationship("Plan")
-    materias = relationship("Materia")
+    @classmethod
+    def from_xml_node(cls, node: ET.Element):
+        fields = {}
+        for col in cls.__table__.columns:
+            xml_value = node.find(col.name)
+            if xml_value is not None and xml_value.text is not None:
+                value = xml_value.text.strip()
+                if isinstance(col.type, Integer):
+                    value = int(value)
+                fields[col.name] = value
+            else:
+                fields[col.name] = None
+        return cls(**fields)
